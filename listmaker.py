f4278a824e71120802accf2166c144b02c5b5328
Gabriel Porto <72639802+existencialvertigo@users.noreply.github.com>
2025-02-20 17:16:17 -0300

    Update listmaker.py

--- listmaker.py
+++ listmaker.py
@@ -4,6 +4,6 @@ def add_codes_to_list(input_string):
     return codes_list
 
 #Exemplo de uso:
-lista = "code1 code2 code3"
+lista = """code1 code2 code3""" #Pode editar aqui diretamente e por os códigos -> Para printar a lista com eles para usar no codepaste.py
 codes = add_codes_to_list(lista)
 print(codes)
